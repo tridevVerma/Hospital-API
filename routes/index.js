b57e3a01d7c012526bdaeab6a3a49e5636b51257
Tridev Verma <tv072000@gmail.com>
2023-05-27 08:05:46 +0530

    Add Default routing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
 router.use("/api", require("./api"));
-
+router.get("*", (req, res) => {
+  return res.send(
+    '<p>This is a COVID-19 Hospital API, To view documentation visit my github page <a href="https://github.com/tridevVerma/Hospital-API">Tridev Verma</a></p>'
+  );
+});
 module.exports = router;
